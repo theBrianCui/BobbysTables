415b2e8e74bcb1fbaa25ada84121fc64644d1508
Brian Cui <brianzcui@gmail.com>
2016-09-07 16:32:17 -0500

    Added session cookie invalidity check

--- src/header.php
+++ src/header.php
@@ -1,4 +1,11 @@
 <?php
+$session_name = session_name();
+if (isset($_COOKIE[$session_name]) and empty($_COOKIE[$session_name])) {
+    // This happens when someone does "clear cookie" in Firebug; it causes session_start()
+    // to trigger a warning. session_start() relies on $_COOKIE[$session_name], thus:
+    unset($_COOKIE[$session_name]);
+}
+session_start();
 session_start();
 
 require_once __DIR__ . '/./config.php';
